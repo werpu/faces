4bbfb0273b523534a5e5e76d9517c5c4dfa0dc94
Bauke Scholtz <balusc@gmail.com>
2022-10-30 11:28:17 -0400

    https://github.com/eclipse-ee4j/mojarra/issues/5150 add IT

--- tck/faces23/facelets/src/test/java/ee/jakarta/tck/faces/test/servlet40/facelets/Spec1102IT.java
+++ tck/faces23/facelets/src/test/java/ee/jakarta/tck/faces/test/servlet40/facelets/Spec1102IT.java
@@ -21,6 +21,7 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
 import ee.jakarta.tck.faces.test.util.arquillian.ITBase;
 
@@ -29,11 +30,22 @@ public class Spec1102IT extends ITBase {
     /**
      * @see com.sun.faces.facelets.component.UIRepeat
      * @see https://github.com/jakartaee/faces/issues/1102
+     * @see https://github.com/eclipse-ee4j/mojarra/issues/5150
      */
     @Test
     public void testSpec1102() throws Exception {
         HtmlPage page = getPage("faces/spec1102.xhtml");
-        
+
+        assertValues(page);
+
+        HtmlSubmitInput button = (HtmlSubmitInput) page.getHtmlElementById("form:submit");
+        page = button.click();
+        webClient.waitForBackgroundJavaScript(60000);
+
+        assertValues(page);
+    }
+
+    private void assertValues(HtmlPage page) {
         assertTrue(page.getHtmlElementById("repeat1").asNormalizedText().equals("123"));
         assertTrue(page.getHtmlElementById("repeat2").asNormalizedText().equals("-3-2-10123"));
         assertTrue(page.getHtmlElementById("repeat3").asNormalizedText().equals("3210-1-2-3"));
@@ -43,5 +55,5 @@ public class Spec1102IT extends ITBase {
         assertTrue(page.getHtmlElementById("repeat7").asNormalizedText().equals("3"));
         assertTrue(page.getHtmlElementById("repeat8").asNormalizedText().equals("0123"));
     }
-
+    
 }
